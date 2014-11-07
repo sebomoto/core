55c7b88e226aa6e1563ff70ae3388c52996da9e8
Joas Schilling <nickvergessen@gmx.de>
2014-11-07 10:52:37 +0100

    Make it possible to cleanPath() absolute Windows paths

--- lib/private/files/filesystem.php
+++ lib/private/files/filesystem.php
@@ -698,10 +698,19 @@ class Filesystem {
 	 * @param bool $stripTrailingSlash
 	 * @return string
 	 */
-	public static function normalizePath($path, $stripTrailingSlash = true) {
+	public static function normalizePath($path, $stripTrailingSlash = true, $isAbsolutePath = false) {
 		if ($path == '') {
 			return '/';
 		}
+
+		// When normalizing an absolute path, we need to ensure that the drive-letter
+		// is still at the beginning on windows
+		$windows_drive_letter = '';
+		if ($isAbsolutePath && \OC_Util::runningOnWindows() && preg_match('#^([a-zA-Z])$#', $path[0]) && $path[1] == ':' && $path[2] == '\\') {
+			$windows_drive_letter = substr($path, 0, 2);
+			$path = substr($path, 2);
+		}
+
 		//no windows style slashes
 		$path = str_replace('\\', '/', $path);
 
@@ -733,7 +742,7 @@ class Filesystem {
 		//normalize unicode if possible
 		$path = \OC_Util::normalizeUnicode($path);
 
-		return $path;
+		return $windows_drive_letter . $path;
 	}
 
 	/**
